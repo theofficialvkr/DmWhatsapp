d9eab99039ae43f41a5154752476d17ff0f951cc
Vijay Kumar <contactvkr@yahoo.com>
2024-09-07 09:15:48 +0530

    Update and rename theofficialvkr-sw.js to js/sw.js

---
+++ js/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker Installed');
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
